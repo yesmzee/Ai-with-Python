765afc56bd54d13452b013f19d083180bd8f849b
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-11 13:52:55 +0500

    update python-intro introduction lines

--- 01-python-fundamentals/python-intro.py
+++ 01-python-fundamentals/python-intro.py
@@ -1,4 +1,6 @@
 # print is a built-in function in Python that outputs text to the console
 # It can be used to display messages, variables, or any other information.
 
-print("Hello Zeeshan ! Welcome to Python programming.")
+print("Hello Everyone this is me !")
+print("This is my first Python program.")
+print("I am learning Python programming.")
