499f9269021f88b091258396ea03df15b94a947f
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-11 13:59:12 +0500

    rename python-intro

--- 01-python-fundamentals/python-intro.py
+++
@@ -1,6 +0,0 @@
-# print is a built-in function in Python that outputs text to the console
-# It can be used to display messages, variables, or any other information.
-
-print("Hello Everyone this is me !")
-print("This is my first Python program.")
-print("I am learning Python programming.")
