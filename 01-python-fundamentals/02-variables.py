d0d7be618d237f86827574980197c8b582e8cb6e
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-12 12:23:15 +0500

    complete 02-variables

--- 01-python-fundamentals/02-variables.py
+++ 01-python-fundamentals/02-variables.py
@@ -1,24 +1,32 @@
 # there are different types of variables in python
+
+
 # this is an integer variable
-# %%
+
 age = 23
 
 # this is a float variable
 score = 95.5
 print("Age:", age, "and Score:", score)
 
-# type() function is used to check the type of a variable
+# type() function is used to check the data-type of a variable
 print("data type of age:", type(age))
 print("data type of score:", type(score))
 
+# print function can also perform operations with variables
+print( score + 5)
+
+
 # print function can also perform string concatenation using the + operator
-# %%
 name = "Zeeshan"
-print("Hello, " + name + "!")
+last_name = "Ali"
+print("Hello, " + name + last_name +" ! ")
 
 # print function can also perform operations on variables
-# %%
 print("Next year, my age will be", age + 1, "years old.")
 
 
-# %%
+# this is a boolean variable
+is_passed = True
+print(is_passed)
+print(type(is_passed))
