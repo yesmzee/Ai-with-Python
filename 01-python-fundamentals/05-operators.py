6dc6ccf863e77ce4fb8cf7049d991d673f070bb5
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-15 12:05:51 +0500

    add topic comparison op & complete

--- 01-python-fundamentals/05-operators.py
+++ 01-python-fundamentals/05-operators.py
@@ -1,21 +1,30 @@
-# OPERATORS 
+# OPERATORS
 # There are two types of operators in Python:
 
 # Unary Operators ---> These operators operate on a single operand
 # Binary Operators ---> These operators operate on two operands
 
-# 1. ARITHMETIC OPERATORS
+# 1. ARITHMETIC or MATHEMATICAL OPERATORS
 
-print( 5 + 3) # addition
+print(5 + 3)  # addition
 print(9 - 4)  # subtraction
-print( 3 * 9) # multiply
-print( 3 / 9) # divide
-print( 17.6 // 3) # floor divide --> removes the decimal part
-
-# floor divides but answer is <int> type if both operands are <int> 
+print(3 * 9)  # multiply
+print(3 / 9)  # divide
+print(17.6 // 3)  # floor divide --> removes the decimal part
+# floor divides but answer is <int> type if both operands are <int>
 # if one of them is <float> result is <float> and only .0 in result not more than .0
 
-print( 10 % 3) #remainder (modulus)
-print( 2 ** 4) #exponent (power)
+print(10 % 3)  # remainder (modulus)
+print(2**4)  # exponent (power)
+
+
+# 2. COMPARISON or RELATIONAL OPERATORS --> always produces a <boolean> result True or False
+
+print(5 == 4)  # equal to
+print(7 != 3)  # not equal to
+print(10 > 2)  # greater than
+print(4 < 1)  # less than
+print(10 >= 8)  # greater than or equal to
+print(13 <= 12)  # less than or equal to
 
 # %%
