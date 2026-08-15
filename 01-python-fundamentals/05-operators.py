bf285dc858cf74b7d51e7519d35ddcd810efa3a0
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-15 11:50:18 +0500

    add 05-operators.py & topic arithmatic opr & complete

---
+++ 01-python-fundamentals/05-operators.py
@@ -0,0 +1,21 @@
+# OPERATORS 
+# There are two types of operators in Python:
+
+# Unary Operators ---> These operators operate on a single operand
+# Binary Operators ---> These operators operate on two operands
+
+# 1. ARITHMETIC OPERATORS
+
+print( 5 + 3) # addition
+print(9 - 4)  # subtraction
+print( 3 * 9) # multiply
+print( 3 / 9) # divide
+print( 17.6 // 3) # floor divide --> removes the decimal part
+
+# floor divides but answer is <int> type if both operands are <int> 
+# if one of them is <float> result is <float> and only .0 in result not more than .0
+
+print( 10 % 3) #remainder (modulus)
+print( 2 ** 4) #exponent (power)
+
+# %%
