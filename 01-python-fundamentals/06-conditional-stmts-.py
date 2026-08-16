94d9da8e30aac286502250564dcd3ca951163da5
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-16 14:56:54 +0500

    rename 06-conditional-stmts.py & add topic if-lse & complete

---
+++ 01-python-fundamentals/06-conditional-stmts-.py
@@ -0,0 +1,18 @@
+# CONDITIONAL STATEMENS
+# conditional statements are used to perform different decisions based on different conditions.
+
+temp = int(input("enter the temperature :"))
+# if-else
+
+# if-else is used when you want to perform :
+# one action if the condition is true and
+# another action if it is false.
+
+# if --> alone used when you want to execute code only if a condition is true
+if temp > 30:
+    print("It is hot today !")
+
+# else --> runs if previous condition is fale.
+else:
+    print("It is not hot today !")
+
