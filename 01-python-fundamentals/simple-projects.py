f62e1d8d4194ab138cd754b7f740f29b3befd166
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-16 13:11:57 +0500

    add simple project & complete

---
+++ 01-python-fundamentals/simple-projects.py
@@ -0,0 +1,32 @@
+# SIMPLE CALCULATOR PROJECT from 01-05 practice files
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+print("Addition of the numbers is :", num1 + num2)
+print("Subtraction of the numbers is :", num1 - num2)
+print("Multiplication of the numbers is :", num1 * num2)
+print("Division of the numbers is :", num1 / num2)
+
+# BASIC INFORMATION SYSTEM
+
+person = {
+    "Name": input("Name"),
+    "Age": int(input("Age")),
+    "Courses": input("Python,AI,Git"),
+}
+print(person)
+
+# BILL CALCULATOR
+
+price = float(input("Enter the price : "))
+quantity = float(input("Enter the quatity"))
+
+print("Total Price is RS:",price * quantity)
+
+# UNIT CONVETER km --> m
+
+km = float(input("Enter the distance in km :"))
+meters = km * 1000
+print("Distance in meters (m) is :",meters)
+
