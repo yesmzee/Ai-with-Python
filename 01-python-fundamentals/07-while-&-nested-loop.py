097ebc1160a544294d10df729e8eb0458a08c6b6
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-19 20:59:29 +0500

    rename 07-while-loop.py & add topic nested loop

--- 01-python-fundamentals/07-while-&-nested-loop.py
+++ 01-python-fundamentals/07-while-&-nested-loop.py
@@ -23,14 +23,35 @@ while count <= 14:
 
 count = 0
 while count < 10:
-    count += 1
-    if count % 2==0 : 
-        continue # skips even nmbrs
+    
+    if count % 2 == 0:
+        continue  # skips even nmbrs
     print(count)
+    count += 1
 
 count = 1
 while count < 11:
     if count == 8:
-        break # break at 8
-    print(count) 
+        break  # break at 8
+    print(count)
     count += 1
+
+# NESTED LOOPS --> means a loop inside another loop
+
+# when outer loop runs once, inner loops runs completely
+for a in range(2):
+    print("outer loop")
+    for z in range(3):
+        print("inner loop")
+
+i = 4
+rows = 4
+cols = 4
+matrix = []
+for r in range(rows):
+    row = []
+    for c in range(cols):
+        row.append(0)
+    matrix.append(row)
+for row in matrix:
+    print(" ".join(map(str, row)))
