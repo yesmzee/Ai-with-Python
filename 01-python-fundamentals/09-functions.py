ecaeda3935f89d7f66fe3682d4a34834664d5200
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-21 18:11:59 +0500

    add topic func. with return value

--- 01-python-fundamentals/09-functions.py
+++ 01-python-fundamentals/09-functions.py
@@ -16,3 +16,12 @@ call("Zeeshan") # Calling Zeeshan
 call("Hassan") # Calling Hassan
 # this above is an example of reuseability 
 
+# FUNCTIONS WITH RETURN VALUE
+
+def sum(a,b):
+    result=a+b
+    return result
+
+answer = sum( 6 , 4 )
+print(answer)
+
