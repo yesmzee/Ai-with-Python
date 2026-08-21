85df25ded5160470f2517f707ac2c99a57697f2d
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-21 18:14:47 +0500

    comments edit for explanation

--- 01-python-fundamentals/01-intro.py
+++ 01-python-fundamentals/01-intro.py
@@ -1,4 +1,5 @@
 # print is a built-in function in Python that outputs text to the console
+
 # It can be used to display messages, variables, or any other information.
 
 print("Hello Everyone this is me !")
