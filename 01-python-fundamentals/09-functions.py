1ac7e2ba82380d25381bc14a48746e07b6bc9a69
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-21 17:29:50 +0500

    add topic parameter & topic function complete

--- 01-python-fundamentals/09-functions.py
+++ 01-python-fundamentals/09-functions.py
@@ -1,8 +1,17 @@
-# FUNCTIONS --> reuseable block of code 
+# FUNCTIONS --> reuseable block of code
 
-def greet(): 
+
+def greet():
     print("Hello, Zeeshan")
 
-greet() #calling a function 
+greet()  # calling a function
 
 
+# with parameter
+
+def call(name):
+    print("Calling", name)
+
+call("Zeeshan") # Calling Zeeshan
+call("Hassan") # Calling Hassan
+# this above is reuseability 
