a6b3999d075bdf34952cc8827d74bc95d9fab248
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-22 20:37:00 +0500

    update comments

--- 01-python-fundamentals/05b-simple-projects.py
+++ 01-python-fundamentals/05b-simple-projects.py
@@ -1,3 +1,7 @@
+
+# Following are practice codes from files 91-intro.py to 05-operators.py
+
+
 # SIMPLE CALCULATOR PROJECT from 01-05 practice files
 
 num1 = float(input("Enter first number : "))
@@ -8,6 +12,7 @@ print("Subtraction of the numbers is :", num1 - num2)
 print("Multiplication of the numbers is :", num1 * num2)
 print("Division of the numbers is :", num1 / num2)
 
+
 # BASIC INFORMATION SYSTEM
 
 person = {
@@ -17,12 +22,14 @@ person = {
 }
 print(person)
 
+
 # BILL CALCULATOR
 
 price = float(input("Enter the price : "))
 quantity = float(input("Enter the quatity"))
 
 print("Total Price is RS:",price * quantity)
+
 
 # UNIT CONVETER km --> m
 
