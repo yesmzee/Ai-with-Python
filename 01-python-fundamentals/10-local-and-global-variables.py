acbd4f6c0840bd1ca11aa6072b580b0bdad34748
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-23 20:43:31 +0500

    add topic local varaible

---
+++ 01-python-fundamentals/10-local-and-global-variables.py
@@ -0,0 +1,12 @@
+# LOCAL  VARIABLES :
+# local variables are defined inside a function and can only be accessed within that function.
+
+
+def my_function():
+    local_variable = "I am a local variable"
+    print(local_variable)
+
+# Calling the function to see the local variable in action
+my_function()
+
+
