0cec3591ad8689cd248c9dde06c8d3ecbd9833b2
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-24 20:52:51 +0500

    update wiht explanation

--- 01-python-fundamentals/02-variables.py
+++ 01-python-fundamentals/02-variables.py
@@ -1,11 +1,21 @@
+# VARIABLES :
+# variables are memory locations for our data
+
+# RULES FOR DEFINING A VARIABLE :
+# start with letter or underscore, not with numbers
+# variables names can't have spaces in between them 
+# keywords or reserved words can not be used as variable name
+# use snakecase-->lower case but with underscore (total_amount ) or 
+# use camelcase-->without spaces or underscore (totalAmount) but second word's first alphabet should be capital
+
 # there are different types of variables in python
 
 
-# this is an integer variable
+# this is an integer variable ( all +ive & -ive ) values
 
 age = 23
 
-# this is a float variable
+# this is a float variable ( all +ive & -ive ) values
 
 score = 95.5
 print("Age:", age, "and Score:", score)
@@ -24,7 +34,7 @@ print( score + 5)
 
 name = "Zeeshan"
 last_name = "Ali"
-print("Hello, " + name + " " + last_name + " ! ")
+print("Hello, " + name + last_name + " ! ")
 
 # print function can also perform operations on variables
 
@@ -32,6 +42,7 @@ print("Next year, my age will be", age + 1, "years old.")
 
 
 # this is a boolean variable
+# boolean has only two values true or false
 
 is_passed = True
 print(is_passed)
