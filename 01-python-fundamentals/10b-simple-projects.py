75f2f8b51d977c64541ba2e5ee95467424485869
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-08-25 21:43:08 +0500

    add program number guessing

--- 01-python-fundamentals/10b-simple-projects.py
+++ 01-python-fundamentals/10b-simple-projects.py
@@ -1,2 +1,23 @@
 # simple programs of topics from 01-intro.py to 10-local-and-global-variables.py
 
+# NUMBER GUESSING GAME :
+
+def guessing_game():
+    number = 7
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess the number: "))
+        attempts += 1
+
+        if guess == number:
+            print("Correct!")
+            print("Attempts:", attempts)
+            break
+        elif guess < number:
+            print("Too low!")
+        else:
+            print("Too high!")
+
+guessing_game()
+
